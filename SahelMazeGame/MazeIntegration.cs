46c2d01eeb811bd69d427ff646a658238644f612
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-08-18 15:59:24 +0430

    Making some chages!

---
+++ SahelMazeGame/MazeIntegration.cs
@@ -0,0 +1,34 @@
+﻿namespace SahelMazeGame
+{
+	public class MazeIntegration : object, AtlasCopco.Integration.Maze.IMazeIntegration
+	{
+		public void BuildMaze(int size)
+		{
+		}
+
+		public bool CausesInjury(int roomId)
+		{
+			return (true);
+		}
+
+		public string GetDescription(int roomId)
+		{
+			return (string.Empty);
+		}
+
+		public int GetEntranceRoom()
+		{
+			return (1);
+		}
+
+		public int? GetRoom(int roomId, char direction)
+		{
+			return (1);
+		}
+
+		public bool HasTreasure(int roomId)
+		{
+			return (true);
+		}
+	}
+}
